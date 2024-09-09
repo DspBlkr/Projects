699023da544b87af6f2338b5c9ffb1a09b99b7af
DspBlkr <23004125@student.barnfield.ac.uk>
2024-09-09 12:15:43 +0100

    testing

--- githublearning/test.lua
+++ githublearning/test.lua
@@ -1,1 +1,16 @@
-
+function string.split(delimiter)
+    local result = { }
+    local from  = 1
+    local delim_from, delim_to = string.find( self, delimiter, from  )
+    while delim_from do
+      table.insert( result, string.sub( self, from , delim_from-1 ) )
+      from  = delim_to + 1
+      delim_from, delim_to = string.find( self, delimiter, from  )
+    end
+    table.insert( result, string.sub( self, from  ) )
+    return result
+  end
+  
+  local string1 = "hi hello testing 23 ! 2 3"
+  
+  local splitString = string.split(string1)
